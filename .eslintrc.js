770f8f1ed9b2f8b9e42287e41f26dbb39a16d893
wrynnsun <wrynnsun@tencent.com>
2021-12-31 18:39:15 +0800

    build: 端口

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:prettier/recommended',
   ],
   root: true,
