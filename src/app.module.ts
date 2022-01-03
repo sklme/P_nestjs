7ad254e243107de433bff9af7e6a8dfb76712c4c
skl <sun.keliang@foxmail.com>
2022-01-03 17:50:58 +0800

    feat: forRoutes可以传入的形式

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DogsModule } from './dogs/dogs.module';
 import { CatsModule } from './cats/cats.module';
 import { LoggerMiddlewareClass } from './middleware/log-class.middleware';
 import { LogFunc } from './middleware/log-provider';
+import { DogsController } from './dogs/dogs.controller';
 
 @Module({
   imports: [DogsModule, CatsModule],
@@ -13,6 +19,16 @@ import { LogFunc } from './middleware/log-provider';
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddlewareClass).forRoutes('dogs');
+    consumer.apply(LoggerMiddlewareClass).forRoutes(
+      // routeinfo
+      {
+        path: 'dogs',
+        method: RequestMethod.GET,
+      },
+      // wildcard
+      'dog*',
+      // Controller
+      DogsController,
+    );
   }
 }
