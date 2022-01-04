07246e11ac044b3bf9aa4abcda118805370db826
skl <sun.keliang@foxmail.com>
2022-01-05 01:28:59 +0800

    feat(pipe): built-in pipe instance

--- src/pipe/pipe.controller.ts
+++ src/pipe/pipe.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Get, ParseIntPipe, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  ParseIntPipe,
+  Query,
+} from '@nestjs/common';
 import { PipeService } from './pipe.service';
 
 @Controller('pipe')
@@ -8,6 +15,29 @@ export class PipeController {
   @Get('built-in')
   builtIn(@Query('id', ParseIntPipe) id: number) {
     //
+    return this.pipeSerivce.handleNumber(id);
+  }
+
+  /**
+   * pipe可以传入一个类，用IOC的方式实例化
+   * 也可以传入一个定制化的示例
+   */
+  @Get('built-in-instance')
+  builtInInstance(
+    @Query(
+      'id',
+      new ParseIntPipe({
+        // errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE,
+        exceptionFactory() {
+          return new HttpException(
+            'URL参数的id参数无法转化为number，请传递别的值',
+            HttpStatus.NOT_ACCEPTABLE,
+          );
+        },
+      }),
+    )
+    id: number,
+  ) {
     return this.pipeSerivce.handleNumber(id);
   }
 }
