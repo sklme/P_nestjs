6067835d4bcfc94db45fe9eec097261f737e79b7
wrynnsun <wrynnsun@tencent.com>
2022-01-04 18:06:36 +0800

    feat: pretty-error

--- src/exceptions/inherite-exception.filter.ts
+++ src/exceptions/inherite-exception.filter.ts
@@ -1,9 +1,10 @@
 /**
  * @file 直接继承原始的exception filter
  */
-
+import PrettyError from 'pretty-error';
 import { ArgumentsHost, Catch } from '@nestjs/common';
 import { BaseExceptionFilter } from '@nestjs/core';
+const pe = new PrettyError();
 
 @Catch()
 export class AllExceptionsFilterInheritFromBaseExceptionFilter extends BaseExceptionFilter {
@@ -11,7 +12,7 @@ export class AllExceptionsFilterInheritFromBaseExceptionFilter extends BaseExcep
     console.log('发生错误啦！笨蛋！');
     console.log('这个exception filter是继承自BaseExceptionFilter！！');
     // 直接调用基类的catch就好了
-    console.log(exception);
+    console.log(pe.render(exception));
     super.catch(exception, host);
   }
 }
