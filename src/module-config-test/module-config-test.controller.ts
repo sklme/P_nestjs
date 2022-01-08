a570dec77ac85862af7d0486363f46dbe4ed2ec6
skl <sun.keliang@foxmail.com>
2022-01-09 03:29:12 +0800

    feat(dynamic module): test

--- src/module-config-test/module-config-test.controller.ts
+++ src/module-config-test/module-config-test.controller.ts
@@ -6,11 +6,15 @@ import {
   ValidationPipe,
 } from '@nestjs/common';
 import { EnvVars } from 'src/config/envShape';
+import { MyConfigModuleService } from 'src/my-config-module/my-config-module.service';
 import { ModuleConfigTestService } from './module-config-test.service';
 
 @Controller('module-config-test')
 export class ModuleConfigTestController {
-  constructor(private service: ModuleConfigTestService) {}
+  constructor(
+    private service: ModuleConfigTestService,
+    private dynamicConfigModule: MyConfigModuleService,
+  ) {}
 
   @Get('get-env')
   @UsePipes(ValidationPipe)
@@ -28,5 +32,11 @@ export class ModuleConfigTestController {
   @Get('github-env')
   githubEnv() {
     return this.service.getGithubEnv();
+  }
+
+  @Get('test-dynamic-config-module')
+  testDynamic() {
+    const isDevelopment = this.dynamicConfigModule.get('DEVELOPMENT_MODE');
+    return isDevelopment;
   }
 }
--- src/module-config-test/module-config-test.module.ts
+++ src/module-config-test/module-config-test.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MyConfigModuleModule } from 'src/my-config-module/my-config-module.module';
 import { ModuleConfigTestController } from './module-config-test.controller';
 import { ModuleConfigTestService } from './module-config-test.service';
 
 // TODO validation的测试
 @Module({
+  imports: [
+    MyConfigModuleModule.register({
+      folder: '',
+    }),
+  ],
   controllers: [ModuleConfigTestController],
   providers: [ModuleConfigTestService],
 })
