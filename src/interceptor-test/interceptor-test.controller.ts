d8120bd5d205a59e66c30d2f828feffdbebd1f9e
skl <sun.keliang@foxmail.com>
2022-01-12 03:23:01 +0800

    feat(interceptor): response mapping

--- src/interceptor-test/interceptor-test.controller.ts
+++ src/interceptor-test/interceptor-test.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, UseInterceptors } from '@nestjs/common';
+import { Controller, Get, Query, UseInterceptors } from '@nestjs/common';
 import { LoggingInterceptor } from './interceptor/logging.interceptor';
+import { TransformInterceptor } from './interceptor/transform.interceptor';
 
 @Controller('interceptor-test')
 export class InterceptorTestController {
@@ -7,5 +8,12 @@ export class InterceptorTestController {
   @UseInterceptors(LoggingInterceptor)
   aspectInterception() {
     return '我最厉害';
+  }
+
+  @Get('response-mapping')
+  @UseInterceptors(TransformInterceptor)
+  responseMapping(@Query('return') returnValue: unknown) {
+    //
+    return returnValue || null;
   }
 }
