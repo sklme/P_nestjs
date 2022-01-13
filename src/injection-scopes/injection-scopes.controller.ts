d084afe483705d6db18ed3abde8fc5816af30b41
wrynnsun <wrynnsun@tencent.com>
2022-01-13 20:46:33 +0800

    feat(injection scopes): custom transient scope provider

--- src/injection-scopes/injection-scopes.controller.ts
+++ src/injection-scopes/injection-scopes.controller.ts
@@ -1,13 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { InjectionScopesService } from './injection-scopes.service';
 
 @Controller('injection-scopes')
 export class InjectionScopesController {
-  constructor(private service: InjectionScopesService) {}
+  constructor(
+    private service: InjectionScopesService,
+    @Inject('custom-provider-with-scope')
+    private customTransientProvider: string,
+  ) {}
+
   @Get('request-scope')
   requestScope() {
     console.log(this.service);
     this.service.printSelf();
     return 'requestScope';
+  }
+
+  @Get('transient-scope')
+  transientScope() {
+    return this.customTransientProvider;
   }
 }
--- src/injection-scopes/injection-scopes.module.ts
+++ src/injection-scopes/injection-scopes.module.ts
@@ -1,9 +1,16 @@
-import { Module } from '@nestjs/common';
+import { Module, Scope } from '@nestjs/common';
 import { InjectionScopesController } from './injection-scopes.controller';
 import { InjectionScopesService } from './injection-scopes.service';
 
 @Module({
   controllers: [InjectionScopesController],
-  providers: [InjectionScopesService],
+  providers: [
+    InjectionScopesService,
+    {
+      provide: 'custom-provider-with-scope',
+      useValue: 'I am Transient Scope Custom provider',
+      scope: Scope.TRANSIENT,
+    },
+  ],
 })
 export class InjectionScopesModule {}
