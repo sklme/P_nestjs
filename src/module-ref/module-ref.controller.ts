24cab960682744be84b102332d1e53f55def9b62
wrynnsun <wrynnsun@tencent.com>
2022-01-14 22:34:59 +0800

    feat(module reference): resolve scoped provider

--- src/module-ref/module-ref.controller.ts
+++ src/module-ref/module-ref.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, OnModuleInit } from '@nestjs/common';
 import { ModuleRef } from '@nestjs/core';
 import { ModuleRefService } from './module-ref.service';
+import { RequstScopeProviderForModuleRefTest } from './providers/request-scope.provider';
 
 @Controller('module-ref')
 export class ModuleRefController implements OnModuleInit {
@@ -8,13 +9,22 @@ export class ModuleRefController implements OnModuleInit {
   constructor(private moduleRef: ModuleRef) {}
 
   onModuleInit() {
+    console.log('获取ModuleRefService...');
     this.service = this.moduleRef.get(ModuleRefService);
-    console.log('获得ModuleRefService');
+    console.log('获取ModuleRefService Done');
     console.log(this.service);
   }
 
   @Get('basic')
   basic() {
     return this.service.test();
+  }
+
+  @Get('dynamic-resolve')
+  async dynamic() {
+    const customProvider: RequstScopeProviderForModuleRefTest =
+      await this.moduleRef.resolve(RequstScopeProviderForModuleRefTest);
+
+    return customProvider.test();
   }
 }
--- src/module-ref/module-ref.module.ts
+++ src/module-ref/module-ref.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ModuleRefController } from './module-ref.controller';
 import { ModuleRefService } from './module-ref.service';
+import { RequstScopeProviderForModuleRefTest } from './providers/request-scope.provider';
 
 @Module({
   controllers: [ModuleRefController],
-  providers: [ModuleRefService],
+  providers: [ModuleRefService, RequstScopeProviderForModuleRefTest],
 })
 export class ModuleRefModule {}
