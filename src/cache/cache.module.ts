303a32d8993cbbacd7e0120167afb47f61eb9c0b
skl <sun.keliang@foxmail.com>
2022-01-16 03:08:32 +0800

    feat(cache): Async configuration

--- src/cache/cache.module.ts
+++ src/cache/cache.module.ts
@@ -1,12 +1,27 @@
 import { Module, CacheModule } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { CacheController } from './cache.controller';
 import { CacheService } from './cache.service';
 
 @Module({
   imports: [
-    CacheModule.register({
-      ttl: 1,
-      max: 20,
+    // sync configuration
+    // CacheModule.register({
+    //   ttl: 1,
+    //   max: 20,
+    // }),
+    // Async configuration
+    CacheModule.registerAsync({
+      inject: [ConfigService],
+      useFactory(configService: ConfigService) {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        const cacheDatabaseConfig = configService.get('database');
+        console.log('缓存数据库的信息是，', cacheDatabaseConfig);
+        return {
+          ttl: 1,
+          max: 20,
+        };
+      },
     }),
   ],
   controllers: [CacheController],
