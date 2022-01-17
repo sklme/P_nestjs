5d2c58d8ea75a9be936a2bdd6c7dae6d4715a96b
wrynnsun <wrynnsun@tencent.com>
2022-01-17 21:01:03 +0800

    feat(schedule): interval and timeout

--- src/task-schedule/task-schedule.service.ts
+++ src/task-schedule/task-schedule.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { Cron } from '@nestjs/schedule';
+import { Cron, CronExpression, Interval, Timeout } from '@nestjs/schedule';
 
 // * * * * * *
 // | | | | | |
@@ -14,18 +14,33 @@ import { Cron } from '@nestjs/schedule';
 export class TaskScheduleService {
   private readonly logger = new Logger(TaskScheduleService.name);
 
-  @Cron('* * * * * *')
+  // @Cron('* * * * * *')
+  // handleCron() {
+  //   this.logger.debug('每秒执行');
+  // }
+
+  // @Cron('*/2 * * * * *')
+  // step2() {
+  //   this.logger.debug('每两秒执行');
+  // }
+
+  // @Cron('1,2,3,4,5,6,20-30 * * * * *')
+  // range() {
+  //   this.logger.debug('某一范围执行');
+  // }
+
+  @Cron(CronExpression.EVERY_10_SECONDS)
   handleCron() {
-    this.logger.debug('每秒执行');
+    this.logger.debug('每5秒执行');
   }
 
-  @Cron('*/2 * * * * *')
-  step2() {
-    this.logger.debug('每两秒执行');
+  @Interval(10000)
+  handleInterval() {
+    this.logger.debug('Called every 10 seconds');
   }
 
-  @Cron('1,2,3,4,5,6,20-30 * * * * *')
-  range() {
-    this.logger.debug('某一范围执行');
+  @Timeout(10000)
+  handleTimeout() {
+    this.logger.warn('我真是太猛了');
   }
 }
