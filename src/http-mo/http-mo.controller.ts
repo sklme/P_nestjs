219223f900f77fbb2d6b4bd81a01cf1e262495fe
wrynnsun <wrynnsun@tencent.com>
2022-01-21 18:37:14 +0800

    todo: 完成http module的学习

--- src/http-mo/http-mo.controller.ts
+++ src/http-mo/http-mo.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('http-mo')
-export class HttpMoController {}
+export class HttpMoController {
+  constructor(private httpService: HttpService) {}
+
+  @Get()
+  basic() {
+    const x = this.httpService.get(
+      'http://localhost:3001/http-mo/http-module-test',
+    );
+    console.log(x);
+    return { x: 1 };
+    // return this.httpService.get<{
+    //   name: string;
+    //   age: number;
+    //   msg: string;
+    // }>('http://localhost:3001/http-mo/http-module-test');
+  }
+
+  @Get('http-module-test')
+  httpModuleTest() {
+    return {
+      name: 'wrynnsun',
+      age: 18,
+      msg: 'for http module test',
+    };
+  }
+}
--- src/http-mo/http-mo.module.ts
+++ src/http-mo/http-mo.module.ts
@@ -1,8 +1,10 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { HttpMoController } from './http-mo.controller';
 import { HttpMoService } from './http-mo.service';
 
 @Module({
+  imports: [HttpModule],
   controllers: [HttpMoController],
   providers: [HttpMoService],
 })
